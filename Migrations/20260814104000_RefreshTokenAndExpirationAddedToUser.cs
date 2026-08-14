f105e2f59544222f1d5133dc1bba9608e453e137
saadmehsud313 <mehsudsaad313@gmail.com>
2026-08-14 15:41:56 +0500

    Add RefreshToken and TokenExpiration fields to User model and update database schema

---
+++ Migrations/20260814104000_RefreshTokenAndExpirationAddedToUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Url_Shortner.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenAndExpirationAddedToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpiration",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenExpiration",
+                table: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -3,7 +3,7 @@ namespace Url_Shortner.Models;
 
 public class User
 {
-    public Guid Id { get; set; }
+    public int  Id { get; set; }
     public string UserName { get; set; } = "";
     public string PasswordHash { get; set; } ="";
     public string Email {get; set;} ="";
