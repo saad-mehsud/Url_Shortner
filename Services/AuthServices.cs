8e25b4be003b7c0063f1999ef90233cdc1f48c28
saadmehsud313 <mehsudsaad313@gmail.com>
2026-08-26 19:57:59 +0500

    feat: implement refresh token creation in AuthServices and remove obsolete token generation method

--- Services/AuthServices.cs
+++ Services/AuthServices.cs
@@ -30,6 +30,14 @@ public class AuthServices(DbConfig context) : IAuthServices
             throw new UnauthorizedException("Invalid email or password");
         }
         RefreshToken? token = await context.RefreshTokens.FirstOrDefaultAsync(tok => tok.User.Id == user.Id && tok.status == Status.Active);
+        
+        if(token is null) {
+            token = new()
+            {
+                userId = user.Id,
+                User = user 
+            };
+        }
         return new TokenResponse()
         {
             AccessToken = CreateToken(user),
@@ -57,16 +65,7 @@ public class AuthServices(DbConfig context) : IAuthServices
         return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
     }
 
-    private async Task<TokenResponse> GenerateTokenAsync(User user)
-    {
-        RefreshToken? token = await context.RefreshTokens.FirstOrDefaultAsync(tok => tok.User.Id == user.Id && tok.status == Status.Active);
-        
-        return new TokenResponse()
-        {
-            AccessToken = CreateToken(user),
-            RefreshToken = await GenerateAndSaveRefreshTokenAsync(token)
-        };
-    }
+   
     public async Task<TokenResponse> RefreshTokenAsync(RefreshRequest refreshRequest)
     {
         User? user = await context.Users.FindAsync(refreshRequest.UserId);
@@ -110,9 +109,9 @@ public class AuthServices(DbConfig context) : IAuthServices
         await context.SaveChangesAsync();
         return  token;
         }
-        catch(Exception exception)
+        catch(Exception)
         {
-            throw exception;
+            throw;
         }
     }
 }
